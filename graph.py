b431542967707f84691e96249cc4583b4a873244
Simone Arcari <53971242+simone-arcari@users.noreply.github.com>
2022-02-09 16:02:23 +0100

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,3 +1,37 @@
+class Queue:
+	def __init__(self):
+		self.coda = []
+    
+	def enqueue(self, elem):
+	# adds one last item to the queue
+	# @param elem: any type of data
+	# @return None
+		self.coda.append(elem)
+    
+	def dequeue(self):
+	# ejects the first element of the queue
+	# @return any type of data
+		return self.coda.pop(0)
+    
+	def first(self):
+	# returns the first element of the queue
+	# @return any type of data
+		return self.coda[0]
+    
+	def last(self):
+	# returns the last element of the queue
+	# @return any type of data
+		return self.coda[-1]
+    
+	def empty(self):
+	# returns true if the queue is empty false otherwise
+	# return bool
+		if len(self.coda) == 0:
+			return True
+		else:
+			return False
+
+
 class Graph:
 
 	def __init__(self):
@@ -21,6 +55,25 @@ class Graph:
 		self.insertNode(u)
 		self.insertNode(v)
 		self.nodes[u][v] = w
+	
+	def graphTraversal(self, r):
+		S = Queue()
+		marker = []
+		
+		S.enqueue(r)
+		marker.append(r)
+		
+		while(not S.empty()):
+		
+			u = S.dequeue()
+			print("node:", u)
+			
+			for v in self.adj(u):
+				print("edge:", u, "->", v)
+				if(not v in marker):
+					marker.append(v)
+					S.enqueue(v)
+					
 
 
 graph = Graph()
@@ -28,5 +81,9 @@ graph = Graph()
 for u,v in [ ('a','b'), ('a','d'), ('b','c'), ('d','a'), ('d','c'), ('d','e'), ('e','c'), ]:
 	graph.insertEdge(u,v)
 
+print("first way to print:")
 for u in graph.V():
-	print(u, "->", graph.adj(u))
+	print("node:", u, "->", graph.adj(u))
+
+print("\nsecond way to print:")
+graph.graphTraversal('a')
